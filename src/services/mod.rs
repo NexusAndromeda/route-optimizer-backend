ae90224d642da9ec09f11b1986fa4401da5191c3
NexusAndromeda <dvnielcueto@outlook.com>
2025-10-19 00:57:13 +0200

    Remove legacy address_validation service (610 lines)

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -5,7 +5,6 @@
 pub mod colis_prive_service;
 pub mod colis_prive_companies_service;
 pub mod geocoding_service;
-pub mod address_validation;
 pub mod address_matching_service;
 pub mod package_processing_service;
 pub mod address_cache_service;
